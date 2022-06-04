5488707d9dcfe0d526d112c1d5c1d206bb143be3
캘리_이지은 <lje4648@afreecatv.com>
2022-06-04 16:23:31 +0900

    compose 1주차

---
+++ week 1-Jetpack Compose basics/Compose_Week1/app/src/main/java/com/example/compose_week1/ui/theme/Theme.kt
@@ -0,0 +1,40 @@
+package com.example.compose_week1.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
+)
+
+private val LightColorPalette = lightColors(
+    surface = Blue,
+    onSurface = androidx.compose.ui.graphics.Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
+)
+
+@Composable
+fun Compose_Week1Theme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
